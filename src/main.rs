7f142fbdd5737496206ed42109fc05bfbba6cd57
Chad Hedstrom <chad.hedstrom@gmail.com>
2019-08-05 03:15:21 -0700

    somewhat cam coords

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,15 @@ impl Position {
     }
 }
 
+// camera position
+//static mut camPosition : Position(x = 0.0, y = -2.0 , z = 0.0);
+
+const CAMPOSITION: Position = Position {
+    x: 0.0,
+    y: -2.0,
+    z: 0.0,
+};
+
 struct Wire {
     start: Position,
     end: Position,
@@ -81,6 +90,13 @@ impl Default for Cube {
             wires: [0; 12],
         }
     }
+    /*
+    fn cube() -> Cube {
+        Cube{
+            wires[0] = Wire
+        }
+    }
+    */
 }
 
 // Projects point onto camera "canvas"
@@ -91,7 +107,30 @@ fn point_on_canvas(pos: Position) -> Position {
     angle_h /= (angle_h.cos()).abs();
     angle_v /= (angle_v.cos()).abs();
 
-    return Position { x: (SCREEN_WIDTH / 2.0 - angle_h * SCREEN_WIDTH / FOV) , y: (SCREEN_HEIGHT/2.0 - angle_v * SCREEN_WIDTH / FOV) , z: 0.0 }
+    return Position { x: (SCREEN_WIDTH / 2.0 - angle_h * SCREEN_WIDTH / FOV) ,
+                      y: (SCREEN_HEIGHT/2.0 - angle_v * SCREEN_WIDTH / FOV) , 
+                      z: 0.0 }
+}
+
+fn to_cam_coords(pos: Position) -> Position{
+  let r_pos = Position{x: 0.0, y: -2.0, z: 0.0};
+  /*
+  //calculating rotation
+  float rx=rPos.x;
+  float ry=rPos.y;
+  float rz=rPos.z;
+  
+  //rotation z-axis
+  rPos.x=rx*cos(-direction)-ry*sin(-direction);
+  rPos.y=rx*sin(-direction)+ry*cos(-direction);
+  
+  //rotation y-axis
+  rx=rPos.x;
+  rz=rPos.z;
+  rPos.x=rx*cos(-rotationY)+rz*sin(-rotationY);
+  rPos.z=rz*cos(-rotationY)-rx*sin(-rotationY);
+  */
+  return r_pos;
 }
 
 
@@ -169,7 +208,57 @@ fn window() {
                 c.transform, g);
 
             // things that move
-            line(game_colors::WHITE, 0.5, [100.0 + hdg as f64, 350.0 + alt as f64, 300.0 + hdg as f64, 350.0 + alt as f64], c.transform, g);
+            line(game_colors::WHITE, 0.5, 
+                    [
+                        100.0 + hdg as f64, 
+                        350.0 + alt as f64, 
+                        300.0 + hdg as f64, 
+                        350.0 + alt as f64
+                        ], 
+                        c.transform, 
+                        g);
+            /*
+            // draw moving things:
+            let c = cube.wires.length
+            for i in 0..c {
+
+                //wires end and start positions transformed to camera coordinates
+                let camPosStart = toCamCoords(cube.wires[i].start);
+                let camPosEnd = toCamCoords(cube.wires[i].end);
+                
+                //projection of start and endpoints to camera
+                let drawStart = pointOnCanvas(camPosStart);
+                let drawEnd = pointOnCanvas(camPosEnd);
+                
+                //drawing lines on screen
+                line(game_colors::WHITE, 0.5, 
+                    [
+                        drawStart.x as f64, 
+                        drawStart.y as f64, 
+                        drawEnd.x as f64, 
+                        drawEnd.y as f64
+                        ], 
+                        c.transform, 
+                        g);
+            }
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         });
     }
 }
